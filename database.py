5ee6ac9f398d10128753e895d5a6e29af3a89c43
fin <haein1012@pusan.ac.kr>
2025-05-21 10:39:37 +0900

    Feat: add api

---
+++ database.py
@@ -0,0 +1,11 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Railway의 DATABASE_URL 환경변수 사용
+DATABASE_URL = os.environ["DATABASE_URL"]
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
