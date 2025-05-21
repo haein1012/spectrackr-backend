47fd25ea22522dade48ec5354877919c1fa43637
fin <haein1012@pusan.ac.kr>
2025-05-21 11:26:14 +0900

    Fix: errors

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Railway의 DATABASE_URL 환경변수 사용
-DATABASE_URL = os.environ.get["DATABASE_URL"]
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
